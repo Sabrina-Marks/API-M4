c67101f5463565c94a2e8af281f824b59c0fb8db
Sabrina-Marks <sabrinamarques0240@gmail.com>
2024-07-22 22:22:39 -0300

    retirando comentários e revisando

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,11 +1,41 @@
 import { Student } from "../models/students.models.js";
 const students = [
-    { id: "8634d265-41c0-4e03-8537-03453273ebf8", name: "José Silva", email: "jose.silva@gmail.com.com", password: "Jq5LbWv9" },
-    { id: "48722908-0f03-48c8-a515-d46e3f441462", name: "Mariana Santos", email: "mariana.santos@gmail.com", password: "hHg3XsYt1" },
-    { id: "d4418ca3-2614-436e-8b09-9c725d768045", name: "Carlos Oliveira", email: "carlos.oliveira@gmail.com", password: "Rf8TkZs2" },
-    { id: "a9950417-65b1-4406-a1b6-5473a60ffb7d", name: "Juliana Pereira", email: "juliana.pereira@gmail.com", password: "Mn7BpQw4" },
-    { id: "01b84133-192b-49b5-8620-76baced3a2b5", name: "Lucas Costa", email: "lucas.costa@gmail.com", password: "Vh2NrPx9" },
-    { id: "931e637d-2167-4089-9278-9a29d327f882", name: "Fernanda Oliveira", email: "fernanda.oliveira@gmail.com", password: "Lp4RsNv6" }
+    {
+        id: "8634d265-41c0-4e03-8537-03453273ebf8",
+        name: "José Silva",
+        email: "jose.silva@gmail.com.com",
+        password: "Jq5LbWv9"
+    },
+    {
+        id: "48722908-0f03-48c8-a515-d46e3f441462",
+        name: "Mariana Santos",
+        email: "mariana.santos@gmail.com",
+        password: "hHg3XsYt1"
+    },
+    {
+        id: "d4418ca3-2614-436e-8b09-9c725d768045",
+        name: "Carlos Oliveira",
+        email: "carlos.oliveira@gmail.com",
+        password: "Rf8TkZs2"
+    },
+    {
+        id: "a9950417-65b1-4406-a1b6-5473a60ffb7d",
+        name: "Juliana Pereira",
+        email: "juliana.pereira@gmail.com",
+        password: "Mn7BpQw4"
+    },
+    {
+        id: "01b84133-192b-49b5-8620-76baced3a2b5",
+        name: "Lucas Costa",
+        email: "lucas.costa@gmail.com",
+        password: "Vh2NrPx9"
+    },
+    {
+        id: "931e637d-2167-4089-9278-9a29d327f882",
+        name: "Fernanda Oliveira",
+        email: "fernanda.oliveira@gmail.com",
+        password: "Lp4RsNv6"
+    }
 ];
 
 
--- src/routes/coursers.routes.js
+++ src/routes/coursers.routes.js
@@ -3,7 +3,6 @@ import { createCousers, getAllCousers, updateCousers, deleteCousers, getCouser }
 
 const cousersRouter = Router();
 
-//post
 
 cousersRouter.post("/cursos", (req, res) => {
     const { title, description, languagem } = req.body;
@@ -12,32 +11,29 @@ cousersRouter.post("/cursos", (req, res) => {
 });
 
 
-//get
+cousersRouter.get("/cursos", (req, res) => {
+    const filter = req.query
+    const listCousers = getAllCousers(filter);
+    return res.status(200).json({ listCousers });
+});
+
 
 cousersRouter.get("/cursos/:id", (req, res) => {
     const { id } = req.params;
     const couser = getCouser(id);
     if (couser) {
-        return res.status(200).json({couser});
+        return res.status(200).json({ couser });
     }
     return res.status(404).send("Id inválido");
 });
 
-
-cousersRouter.get("/cursos", (req, res) => {
-    const filter = req.query
-    const listCousers = getAllCousers(filter);
-    return res.status(200).json({listCousers});
-});
-
-//patch
 
 cousersRouter.patch("/cursos", (req, res) => {
     const { id, description } = req.body;
     const update = updateCousers(id, description);
     res.status(200).json({ update });
 });
-// delete 
+
 
 cousersRouter.delete("/cursos/:id", (req, res) => {
     const { id } = req.params;
