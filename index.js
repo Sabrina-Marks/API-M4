f6d0446d4c4170c4e7eb073cfdf878505103f6ef
Sabrina-Marks <sabrinamarques0240@gmail.com>
2024-09-22 11:36:58 -0300

    atualização para deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { studentRouter } from "./routes/students.routes.js";
-import { cousersRouter } from "./routes/coursers.routes.js";
+import cors from 'cors'
+import { studentRouter } from "./src/routes/students.routes.js";
+import { cousersRouter } from "./src/routes/coursers.routes.js";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(cors())
 app.use(studentRouter);
 app.use(cousersRouter);
 
